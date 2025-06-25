e41071b8d0d6f7f0f1f7c211e5bf8edda7d6bab8
ᓚᘏᗢ <choiashley0207@gmail.com>
2025-06-25 17:48:13 +0900

    Fix: 이메일 인증 방식 오류 수정

--- Assets/Scripts/AccountManager/LoginAndResetPasswordManager.cs
+++ Assets/Scripts/AccountManager/LoginAndResetPasswordManager.cs
@@ -10,10 +10,6 @@ public class LoginAndResetPasswordManager : MonoBehaviour, IAccountManager
     
     [Header("Reset Password")]
     public TMP_InputField resetPasswordEmailInputField;
-    public TMP_InputField resetPasswordVerifyCodeInputField;
-    
-    private bool _isEmailVerified = false;
-    private string _verifiedEmail;
 
     private void Awake()
     {
@@ -21,77 +17,11 @@ public class LoginAndResetPasswordManager : MonoBehaviour, IAccountManager
         ResetResetPasswordEmailText();
     }
     
-    
-    public void OnSendEmailVerificationButtonClick()
-    {
-        string email = resetPasswordEmailInputField.text;
-
-        if (IsNull(email))
-        {
-            AlertSystem.Instance.Error("email " + NotificationTexts.TextNullError);
-            return;
-        }
-
-        StartCoroutine(Api_SendEmailVerification.Send(email, (status, message) =>
-        {
-            if (status)
-            {
-                AlertSystem.Instance.Notice(message);
-                _verifiedEmail = email;
-            }
-            else
-            {
-                AlertSystem.Instance.Error(message);
-                _verifiedEmail = null;
-            }
-        }));
-
-    }
-
-    public void OnVerifyEmailButtonClick()
-    {
-        string email = resetPasswordEmailInputField.text;
-        string verifyCode = resetPasswordVerifyCodeInputField.text;
-
-        if (IsNull(email) || IsNull(verifyCode))
-        {
-            AlertSystem.Instance.Error("email or verifyCode " + NotificationTexts.TextNullError);
-            return;
-        }
-
-        StartCoroutine(Api_VerifyEmail.Send(email, verifyCode, (status, message) =>
-        {
-            if (status)
-            {
-                _isEmailVerified = true;
-                AlertSystem.Instance.Notice(message);
-            }
-            else
-            {
-                _isEmailVerified = false;
-                AlertSystem.Instance.Error(message);
-            }
-        }));
-    }
 
     public void OnFindPasswordButtonClick()
     {
-        if (!_isEmailVerified)
-        {
-            AlertSystem.Instance.Error(NotificationTexts.EmailVerifyError);
-            return;
-        }
-
         string email = resetPasswordEmailInputField.text;
-
-        if (email != _verifiedEmail)
-        {
-            _isEmailVerified = false;
-            _verifiedEmail = null;
-            AlertSystem.Instance.Error(NotificationTexts.verifiedEmailChanged);
-            return;
-        }
-
+        
         StartCoroutine(Api_ResetPassword.Send(email, (status, message) =>
         {
             if (status)
