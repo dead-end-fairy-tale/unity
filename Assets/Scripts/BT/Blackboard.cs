651d1ab3e06ba7eeb50b55f881c395b715dc0381
Doyun Kim <82309982+ehdbs0903@users.noreply.github.com>
2025-07-13 20:14:07 +0900

    feat(*) : Robo 애니메이션 추가

--- Assets/Scripts/BT/Blackboard.cs
+++ Assets/Scripts/BT/Blackboard.cs
@@ -1,15 +1,23 @@
+using System;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class Blackboard
 {
     private readonly Dictionary<string, object> _data = new Dictionary<string, object>();
+    public event Action<string, object> OnValueChanged;
 
     public void Set<T>(string key, T value)
     {
+        if (_data.TryGetValue(key, out var existingObj) &&
+            existingObj is T existingValue &&
+            EqualityComparer<T>.Default.Equals(existingValue, value))
+        {
+            return;
+        }
+        
         _data[key] = value;
+        OnValueChanged?.Invoke(key, value);
     }
-    // public void Set<T>(string key, T value) => _data[key] = value;
 
     public T Get<T>(string key)
         => _data.TryGetValue(key, out var o) && o is T t ? t : default;
